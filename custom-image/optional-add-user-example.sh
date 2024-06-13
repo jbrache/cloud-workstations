69639f61a2ae1765f45741a4686f8f85b8b67967
Jose Brache <jbrache@google.com>
2024-06-13 13:15:38 +0000

    initial commit

---
+++ custom-image/optional-add-user-example.sh
@@ -0,0 +1,12 @@
+export groups=docker,sudo,users
+useradd -m jose -G $groups --shell /bin/bash > /dev/null
+passwd -d jose >/dev/null
+# sudo su jose - # wrong
+# The dash should go before the username, otherwise it doesn't
+# load the user profile which is why the profile.d script didn't get executed
+sudo su - jose # right
+
+# Above works OK with --shell /bin/bash but not with --shell /use/bin/zsh
+# If using another shell, i.e. zsh would need to adapt the this sample,
+# the startup portions still be OK. Would need to plug something into
+# /etc/zprofile or something if we want to support he and hoc command line useradd scenario
