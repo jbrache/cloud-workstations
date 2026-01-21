e12955ef6b3357200125f7d2436bdbe326238b67
Jose Brache <jbrache@google.com>
2026-01-21 19:32:12 +0000

    add xpra version

---
+++ custom-image-ds/workstation-container-antigravity-v3-xpra/assets/etc/workstation-startup.d/110_start-xpra-xterm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "Starting Xpra with xterm..."
+
+# https://medium.com/@roken/extending-google-cloud-workstations-containers-to-run-any-gui-based-program-133d0f905106
+runuser user -c -l "xpra start --bind-tcp=0.0.0.0:80 --min-port=80 --html=on --systemd-run=yes --daemon=no --dbus-launch='' --dbus-control=no --start-child-after-connect=xterm"
