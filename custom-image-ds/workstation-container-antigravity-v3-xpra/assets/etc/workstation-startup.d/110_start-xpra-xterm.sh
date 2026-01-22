ea55e450373a49f40c91ec56c0a8ad64d827bec7
Jose Brache <jbrache@google.com>
2026-01-22 04:15:55 +0000

    change from gnome to mate

--- custom-image-ds/workstation-container-antigravity-v3-xpra/assets/etc/workstation-startup.d/110_start-xpra-xterm.sh
+++ custom-image-ds/workstation-container-antigravity-v3-xpra/assets/etc/workstation-startup.d/110_start-xpra-xterm.sh
@@ -17,4 +17,11 @@
 echo "Starting Xpra with xterm..."
 
 # https://medium.com/@roken/extending-google-cloud-workstations-containers-to-run-any-gui-based-program-133d0f905106
-runuser user -c -l "xpra start --bind-tcp=0.0.0.0:80 --min-port=80 --html=on --systemd-run=yes --daemon=no --dbus-launch='' --dbus-control=no --start-child-after-connect=xterm"
+# [ Option 1 ] - xterm
+# runuser user -c -l "xpra start --bind-tcp=0.0.0.0:80 --min-port=80 --html=on --systemd-run=yes --daemon=no --dbus-launch='' --dbus-control=no --start-child-after-connect=xterm"
+
+# [ Option 2 ] - Gnome
+# runuser user -c -l "xpra start-desktop :7 --bind-tcp=0.0.0.0:80 --min-port=80 --html=on --systemd-run=yes --daemon=no --dbus-launch='' --dbus-control=no --start-child=gnome-session"
+
+# [ Option 3 ] MATE Desktop
+runuser user -c -l "xpra start-desktop :7 --bind-tcp=0.0.0.0:80 --min-port=80 --html=on --systemd-run=yes --daemon=no --dbus-launch='' --dbus-control=no --start-child=mate-session"
